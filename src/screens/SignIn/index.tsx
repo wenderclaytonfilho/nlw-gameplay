94c00d4f1c3d40c83583906914f68a48b00a33ce
wenderclaytonfilho <rednew2727@gmail.com>
2024-05-24 11:51:02 -0300

    fetch:project initialzied

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {styles} from './style';
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Sign In
+      </Text>
+    </View>
+  );
+}
