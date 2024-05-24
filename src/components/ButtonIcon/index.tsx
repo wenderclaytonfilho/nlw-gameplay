30cbcc7e8aff1d0620df9478f64ea84ce15e43c0
wenderclaytonfilho <rednew2727@gmail.com>
2024-05-24 12:49:17 -0300

    fetch: Sign In screen almost done

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,23 @@
+import { Text, Image, View, TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+import DiscorImg from '../../assets/discord.png';
+import { styles } from './style';
+
+type Props = TouchableOpacityProps & {
+    title: string;
+}
+
+export function ButtonIcon({ title, ...rest }: Props) {
+
+    return (
+        <TouchableOpacity
+            style={styles.container}
+            {...rest}
+        >
+            <View style={styles.iconWrapper}>
+                <Image source={DiscorImg} style={styles.icon}></Image>
+            </View>
+            <Text style={styles.title}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,41 @@
-import { Text, View } from 'react-native';
-import {styles} from './style';
+import {
+  Text,
+  View,
+  Image,
+  StatusBar,
+} from 'react-native';
+
+import IllustrationImg from '../../assets/illustration.png';
+
+import { styles } from './style';
+import { ButtonIcon } from '../../components/ButtonIcon';
 
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <Text>
-        Sign In
-      </Text>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent 
+        />
+      <Image source={IllustrationImg}
+        style={styles.image}
+        resizeMode='stretch'
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas{`\n`}
+          facilmente!
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{`\n`}
+          favoritos com seus amigos.
+        </Text>
+        <ButtonIcon title='Entre com o Discord' activeOpacity={.7}/>
+      </View>
+
     </View>
   );
 }
