b27fe80435e974d9f341a69132490e02d8e4308c
wenderclaytonfilho <rednew2727@gmail.com>
2024-05-27 16:20:20 -0300

    fix: Fixed scrollbar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -14,28 +14,88 @@ export function Home() {
 
     const appointments = [
         {
-            id:'1',
-            guild:{
-                id:'1',
-                name:'Ghost',
-                icon:null,
-                owner:true
+            id: '1',
+            guild: {
+                id: '1',
+                name: 'Ghost',
+                icon: null,
+                owner: true
             },
-            category:'1',
-            date:'22/06 às 20:40h',
-            description:'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
         },
         {
-            id:'2',
-            guild:{
-                id:'1',
-                name:'TheBoys',
-                icon:null,
-                owner:true
+            id: '2',
+            guild: {
+                id: '1',
+                name: 'TheBoys',
+                icon: null,
+                owner: true
             },
-            category:'1',
-            date:'27/05 às 18:00h',
-            description:'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+            category: '1',
+            date: '27/05 às 18:00h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+        },
+        {
+            id: '3',
+            guild: {
+                id: '1',
+                name: 'TheBoys',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '27/05 às 18:00h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+        },
+        {
+            id: '4',
+            guild: {
+                id: '1',
+                name: 'TheBoys',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '27/05 às 18:00h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+        },
+        {
+            id: '5',
+            guild: {
+                id: '1',
+                name: 'TheBoys',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '27/05 às 18:00h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+        },
+        {
+            id: '6',
+            guild: {
+                id: '1',
+                name: 'TheBoys',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '27/05 às 18:00h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+        },
+        {
+            id: '7',
+            guild: {
+                id: '1',
+                name: 'TheBoys',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '27/05 às 18:00h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
         }
     ]
 
@@ -44,36 +104,31 @@ export function Home() {
     }
     return (
         <Background>
-            <View>
-                <View style={styles.header}>
-                    <Profile />
-                    <ButtonAdd />
-                </View>
-                <View>
-                    <CategorySelect
-                        categorySelected={category}
-                        setCategory={handleCategorySelect}
-                    />
-                    <View style={styles.content}>
-                        <ListHeader
-                        title='Partidas Agendadas'
-                        subtitle='Total : 2'
-                        />
 
-                        <FlatList
-                            style={styles.matches}
-                            showsVerticalScrollIndicator={false}
-                            data={appointments}
-                            keyExtractor={item => item.id}
-                            ItemSeparatorComponent={()=> <ListDivider/>}
-                            renderItem ={({item}) => (
-                                <Appointment
-                                    data={item}/>
-                            )}
-                        />
-                    </View>
-                </View>
+            <View style={styles.header}>
+                <Profile />
+                <ButtonAdd />
             </View>
+            <CategorySelect
+                categorySelected={category}
+                setCategory={handleCategorySelect}
+            />
+                <ListHeader
+                    title='Partidas Agendadas'
+                    subtitle='Total : 2'
+                />
+
+            <FlatList
+                style={styles.matches}
+                showsVerticalScrollIndicator={false}
+                data={appointments}
+                keyExtractor={item => item.id}
+                ItemSeparatorComponent={() => <ListDivider />}
+                renderItem={({ item }) => (
+                    <Appointment
+                        data={item} />
+                )}
+            />
         </Background>
     );
 }
