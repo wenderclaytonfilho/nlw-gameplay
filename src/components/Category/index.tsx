c95456bfce3cb253ff8ba12c0c087f9997d298c7
wenderclaytonfilho <rednew2727@gmail.com>
2024-05-28 16:43:29 -0300

    fix:CategorySelect component

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -11,31 +11,40 @@ type Props = RectButtonProps & {
     title: string;
     icon: React.FC<SvgProps>;
     checked?: boolean;
+    hasCheckbox?:boolean;
 }
 
 export function Category({
     title,
     icon: Icon,
     checked = false,
+    hasCheckbox=false,
     ...rest
 }: Props) {
-    const { secondary50, secondary70 } = theme.colors;
+    const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
     return (
         <RectButton{...rest}>
             <LinearGradient
                 style={styles.container}
                 colors={[secondary50, secondary70]}
             >
-                <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-                    <View style={checked ? styles.checked : styles.check}/>
-                    <Icon
-                            width={48}
-                            height={48}
-                        />
-                    <Text style={styles.title}>
-                        {title}
-                    </Text>
-                </View>
+                <LinearGradient
+                    style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+                    colors={[checked ? secondary85 : secondary50, secondary40]}>
+                        {
+                            hasCheckbox &&
+                            <View style={checked ? styles.checked : styles.check} />
+                        }
+                
+                <Icon
+                    width={48}
+                    height={48}
+                />
+                
+                <Text style={styles.title}>
+                    {title}
+                </Text>
+                </LinearGradient>
             </LinearGradient>
         </RectButton>
     );
