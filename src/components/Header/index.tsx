bd63a3a4168769b2930ec12d6d520f3cfa78fed0
wenderclaytonfilho <rednew2727@gmail.com>
2024-05-30 12:06:19 -0300

    fetch:header component finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,20 +5,29 @@ import { theme } from '../../global/styles/theme';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons'
 import { style } from './style';
+import { useNavigation } from '@react-navigation/native';
 
 type Props = {
     title: string;
     action?: ReactNode;
 }
 
+
 export function Header({ title, action }: Props) {
+
+    const navigation = useNavigation();
+
+    function handleGoBack(){
+        navigation.goBack();
+    }
+    
     const { secondary100, secondary40, heading } = theme.colors;
     return (
         <LinearGradient
             colors={[secondary100, secondary40]}
             style={style.container}
         >
-            <BorderlessButton>
+            <BorderlessButton onPress={handleGoBack}>
                 <Feather
                     name='arrow-left'
                     size={24}
